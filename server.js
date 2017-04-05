67670d4ec614e12c64f9102c778580c8589c73cb
berazumis124 <berazumis@gmail.com>
2017-04-05 21:29:37 +0300

    Added UPDATE

--- server.js
+++ server.js
@@ -66,6 +66,36 @@ app.delete('/todos/:id', function (req, res) {
     }
 });
 
+// PUT /todos/:id
+
+app.put('/todos/:id', function (req, res) {
+    var todoIdDel = parseInt(req.params.id, 10);
+    var matchedIdDel = _.findWhere(todos, {id: todoIdDel});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    
+    if (!matchedIdDel) {
+        return res.status(404).send();
+    }
+    
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+       // console.log(validAttributes.completed);
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    
+    if (body.hasOwnProperty('description') && _.isString(body.description) && (body.description.trim().length > 0)) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+    
+    _.extend(matchedIdDel, validAttributes);
+    res.json(matchedIdDel);
+    
+});
+
 app.listen(PORT, function (){
     console.log('Express listening on port ' + PORT + '!');
 });
