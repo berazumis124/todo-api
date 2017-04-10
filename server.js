b15869c2e75c4ddcbe344ddbb7249920af50b0ae
berazumis124 <berazumis@gmail.com>
2017-04-10 11:47:37 +0300

    Converted PU /todos:id to use sequelize

--- server.js
+++ server.js
@@ -87,30 +87,33 @@ app.delete('/todos/:id', function (req, res) {
 // PUT /todos/:id
 
 app.put('/todos/:id', function (req, res) {
-    var todoIdDel = parseInt(req.params.id, 10);
-    var matchedIdDel = _.findWhere(todos, {id: todoIdDel});
+    var todoId = parseInt(req.params.id, 10);
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
     
-    if (!matchedIdDel) {
-        return res.status(404).send();
-    }
-    
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
+
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
        // console.log(validAttributes.completed);
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).send();
     }
     
-    if (body.hasOwnProperty('description') && _.isString(body.description) && (body.description.trim().length > 0)) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
     
-    _.extend(matchedIdDel, validAttributes);
-    res.json(matchedIdDel);
+    db.todo.findById(todoId).then(function (todo) {
+        if (todo) {
+            todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON());
+            }, function (e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function() {
+        res.status(500).send();
+    })
     
 });
 
